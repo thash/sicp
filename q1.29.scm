268374bd16a54375ea0533715fa1eda05ad5b8a0
takuyahashimoto <takuya21hashimoto@gmail.com>
2012-01-15 20:46:18 +0900

    q1.30. fill blanks. finish prep for 2012-01-16

--- q1.29.scm
+++ q1.29.scm
@@ -7,10 +7,19 @@
   (define h (/ (- b a) n))
   (define (y k) (f (+ a (* k h))))
   (define (term i)
-    (+ (y (- (* 2 i) 2))
-       (* 4 (y (- (* 2 i) 1)))
-       (y (* 2 i))))
+    (+ (y (- (* 2 i) 2))       ;  y_(2i-2)
+       (* 4 (y (- (* 2 i) 1))) ; 4y_(2i-1)
+       (y (* 2 i))))           ;  y_(2i)
   (define (next i) (+ i 1))
+  (trace f)
+  (trace y)
   (/ (* h (sum term 1 next (/ n 2))) 3))
 
+
+
+(print (integral cube 0 1 0.01))
+;(print (integral cube 0 1 0.001)) ; don't work...
+
 (print (simpson cube 0 1 100))
+;(print (simpson cube 0 1 1000))
+
---
+++ q1.30.scm
@@ -0,0 +1,20 @@
+(add-load-path ".")
+(load "my_defs")
+(load "sec1.3.1.sum")
+
+; QUESTION
+; (define (sum term a next b)
+;   (define (iter a result)
+;     (if <??>
+;         <??>
+;         (iter <??> <??>)))
+;   (iter <??> <??>))
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        ;(iter (next a) (term a)))) ;my answer
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
