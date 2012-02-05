bcc4eb767e1070b0ea9e4880b93bb6d3efed144f
takuyahashimoto <takuya21hashimoto@gmail.com>
2012-02-05 14:21:45 +0900

    Pascal's triangle.

--- q1.12.scm
+++ q1.12.scm
@@ -1,1 +1,18 @@
-(define )
+(add-load-path ".")
+(load "my_defs")
+
+(define (pascal n k)
+  (cond ((and (= n 1) (= k 1)) 1)
+        ((= k 0) 0)
+        ((< n k) 0)
+        (else (+ (pascal (- n 1) (- k 1))
+                 (pascal (- n 1) k)))
+  ))
+
+(trace pascal)
+(print (pascal 1 1)) ;=> 1
+(print (pascal 2 1)) ;=> 1
+(print (pascal 3 2)) ;=> 2
+(print (pascal 5 3)) ;=> 6
+
+
