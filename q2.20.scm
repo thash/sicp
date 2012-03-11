0d3452f7c94dfcef1a32d4b042d310206c599c54
Hash <takuya21hashimoto@gmail.com>
2012-03-12 08:51:58 +0900

    q2.20 不要な抽象化をした気がしないでもない

---
+++ q2.20.scm
@@ -0,0 +1,34 @@
+; 任意の数の引数を取る手続きの定義
+; (define (f x y . z) <body>)
+; (f 1 2 3 4 5 6)
+; => x=1, y=2, z=(3 4 5 6)
+
+; "1つかそれを越える個数の整数を取り、先頭と同じ偶奇性を持つ引数のリストを返す手続き"
+(define (same-parity x . y)
+  (define (true-list proc vals)
+    (define (iter vals result)
+      (if (null? vals)
+        result
+        (if (proc (car vals))
+          (iter (cdr vals) (append result (list (car vals))))
+          (iter (cdr vals) result))))
+    (iter vals ()))
+
+  (cond ((even? x) (true-list even? (cons x y)))
+        ((odd? x) (true-list odd? (cons x y)))))
+
+; 与えられたprocがtrueのものだけのリストを返す手続きtrue-listを内部で定義
+
+
+
+; 引数のリストから偶数だけ返す手続き(↑の部分)
+; (define (returneven vals)
+;   (define (iter vals result)
+;     (if (null? vals)
+;       result
+;       (if (even? (car vals))
+;         (iter (cdr vals) (append result (list (car vals))))
+;         ; (cons (car vals) result) だと逆順になる
+;         (iter (cdr vals) result))))
+;   (iter vals ()))
+
