78d6bf1d43ce2cd80f7b782669a352fc17f332de
Hash <takuya21hashimoto@gmail.com>
2012-04-01 16:24:20 +0900

    need review: q2.36.scm

---
+++ q2.36.scm
@@ -0,0 +1,22 @@
+; accumulate-n は第三引数として「"すべてが同数の要素からなる並び"の並び」を取る他はaccumulateと同じ。
+; accumulationとして指定した手続きを、並びの全ての第一要素、第二要素...と作用させ、結果の並びを返す。
+; Rubyで言うzip?
+
+; 例
+(define s (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+(accumulate-n + 0 s)
+; => (22 26 30)
+
+; 再びaccumulate定義
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op initial seqs)
+  (if (null? (car seqs)) ; 一個下を見なければならない
+    ()
+    (cons (accumulate op initial (map car seqs))
+          (accumulate-n op initial (map cdr seqs)))))
+
