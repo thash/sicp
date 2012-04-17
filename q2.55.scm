8b034a8aa786336c38309bbf068c4dea7dd4cf89
Hash <takuya21hashimoto@gmail.com>
2012-04-17 10:09:28 +0900

    2012-04-16-study

--- q2.55.scm
+++ q2.55.scm
@@ -7,3 +7,16 @@
 ; gosh> (cdr ''aaaa)
 ; (aaaa)
 
+; 勉強会でメモ。
+; 'はquote手続きの省略形(?)である。
+(quote (1 2 3 4))
+; => (1 2 3 4)
+
+; したがって''aaaはinterpreterにより
+; (quote (quote aaa)) と解釈される。
+(quote (quote abbb))
+; => 'abbb
+
+; 印字される形と実際のデータ型(?)が対応してない、ということ？
+
+
