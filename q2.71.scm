de4acb2ae2094e775508323ca5930413118850ea
Hash <takuya21hashimoto@gmail.com>
2012-05-14 09:13:36 +0900

    q2.71, 2.72 (skip

---
+++ q2.71.scm
@@ -0,0 +1,7 @@
+; N = 5のHuffman木。
+; http://www.evernote.com/shard/s11/sh/85a55e3e-9663-4aa1-80aa-24457fcd08a1/b5f530adb83ec3b48202789cf4fb8252
+;
+; 一般に最高頻度のものが 1 bit, 
+;       最低頻度のものが N-1 bit?
+;
+; 2^(n-1)という特徴が生きてない回答なので違いそう。
