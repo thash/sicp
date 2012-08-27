ce16e7a36ab78fee121f502e14b032a2ba6bcd63
Hash <takuya21hashimoto@gmail.com>
2012-08-27 10:32:05 +0900

    q3.50 -- stream-map

---
+++ q3.50-test.scm
@@ -0,0 +1,19 @@
+(load "./my_defs")
+(use gauche.test)
+
+(test-start "q3.50")
+(load "./q3.50")
+
+(test-section "stream-map | empty")
+(define s (stream-map sqrt the-empty-stream))
+(display s)
+(newline)
+(display-stream s)
+
+(test-section "stream-map | enumerate-interval")
+(define x (stream-enumerate-interval 50 100))
+(define t (stream-map sqrt x))
+(display t)
+(newline)
+(display-stream t)
+
---
+++ q3.50.scm
@@ -0,0 +1,10 @@
+(load "./stream.scm")
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+    the-empty-stream
+    (cons-stream
+      (apply proc (map stream-car argstreams))
+      (apply stream-map
+             (cons proc (map stream-cdr argstreams))))))
+
