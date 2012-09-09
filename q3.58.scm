752ead0b68ff7d1cb13ad36f9cbeeb50cdafcf78
Hash <takuya21hashimoto@gmail.com>
2012-09-10 02:19:17 +0900

    study 2012-09-03

--- q3.58.scm
+++ q3.58.scm
@@ -17,4 +17,15 @@
 ;; num/denを計算したときの小数点以下の数字列が表示される。
 ;; radixは奇数. 2にすると2進数扱い
 
+; gosh> (display-stream-n (expand 1 7 10) 10)
+; 1, 4, 2, 8, 5, 7, 1, 4, 2, done
+; gosh> (/ 1.0 7)
+; 0.14285714285714285
+
+;; n進数
+; gosh> (display-stream-n (expand 1 7 7) 10)
+; 1, 0, 0, 0, 0, 0, 0, 0, 0, done
+; gosh> (display-stream-n (expand 1 7 6) 10)
+; 0, 5, 0, 5, 0, 5, 0, 5, 0, done
+
 
