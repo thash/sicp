78ebecd623d06843024b8e8eda15b2c1d96d26fe
Hash@nona <takuya21hashimoto@gmail.com>
2012-11-07 19:17:05 +0900

    streams as lazy lists. inside sec4.2-lazy.scm

--- sec4.2-lazy.scm
+++ sec4.2-lazy.scm
@@ -742,3 +742,64 @@
         (else obj)))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4.2.3. 遅延評価リストとしてのストリーム
+;;   遅延評価において, consをnon-strictに定義すればストリームはリストそのものになる.
+;;
+;;   cons, car, cdrをprimitive-procedureの座から引き摺り下ろせ！
+;;   => どうせdriver-loopに入れないと動かないし,
+;;      primitive-procedureのcons,car,cdrが上書きされるなら同じファイルでも良いか.
+
+;; 注意: 以下はdriver-loopを走らせてからdefすること.めどい
+
+; (driver-loop)
+
+;    (define (cons x y)
+;      (lambda (m) (m x y)))
+;
+;    (define (car z)
+;      (z (lambda (p q) p)))
+;
+;    (define (cdr z)
+;      (z (lambda (p q) q)))
+;
+;    ;; sample procs
+;
+;    (define (list-ref items n)
+;      (if (= n 0)
+;        (car items)
+;        (list-ref (cdr items) (- n 1))))
+;
+;    (define (map proc items)
+;      (if (null? items)
+;        '()
+;        (cons (proc (car items))
+;              (map proc (cdr items)))))
+;
+;    (define (scale-list items factor)
+;      (map (lambda (x) (* x factor))
+;           items))
+;
+;    (define (add-lists list1 list2)
+;      (cond ((null? list1) list2)
+;            ((null? list2) list1)
+;            (else (cons (+ (car list1) (car list2))
+;                        (add-lists (cdr list1) (cdr list2))))))
+;
+;    (define ones (cons 1 ones))
+;    (define integers (cons 1 (add-lists ones integers)))
+;
+;    ;; これらの遅延リストは3章のストリームより遅延度が高い. carも遅延されてる.
+;    ;; メリット: 3.5.4. でdelayかけまくっていたところが, リストがすべて遅延することによりすっきりする.
+;
+;    (define (integral integrand initial-value dt)
+;      (define int
+;        (cons initial-value
+;              (add-lists (scale-list integrand dt)
+;                         int)))
+;      int)
+;
+;    (define (solve f y0 dt)
+;      (define y (integral dy y0 dt))
+;      (define dy (map f y))
+;      y)
