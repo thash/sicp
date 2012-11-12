4a4491074f231eedcc8e9a011ccf1319c3de9c53
Hash <takuya21hashimoto@gmail.com>
2012-11-12 14:04:59 +0900

    to use amb ... 1. comment out examples, 2. add let in analyze

--- sec4.3-nondeterministic.scm
+++ sec4.3-nondeterministic.scm
@@ -62,26 +62,26 @@
 ;;    evalの中身は式に応じた場合分けの形を取る.
 
 ;; 注意: 4.1.7. にてanalyze版に上書きされる
-(define (eval exp env)
-  (cond ((self-evaluating? exp) exp)
-        ((variable? exp) (lookup-variable-value exp env))
-        ((quoted? exp) (text-of-quotation exp))
-        ((assignment? exp) (eval-assignment exp env))
-        ((definition? exp) (eval-definition exp env))
-        ((if? exp) (eval-if exp env))
-        ((let? exp) (eval (let->combination exp) env))
-        ((lambda? exp)
-         (make-procedure (lambda-parameters exp)
-                         (lambda-body exp)
-                         env))
-        ((begin? exp)
-         (eval-sequence (begin-actions exp) env))
-        ((cond? exp) (eval (cond->if exp) env))
-        ((application? exp)
-         (apply (eval (operator exp) env)
-                (list-of-values (operands exp) env)))
-        (else
-          (error "Unknown expression type -- EVAL" exp))))
+;(define (eval exp env)
+;  (cond ((self-evaluating? exp) exp)
+;        ((variable? exp) (lookup-variable-value exp env))
+;        ((quoted? exp) (text-of-quotation exp))
+;        ((assignment? exp) (eval-assignment exp env))
+;        ((definition? exp) (eval-definition exp env))
+;        ((if? exp) (eval-if exp env))
+;        ((let? exp) (eval (let->combination exp) env))
+;        ((lambda? exp)
+;         (make-procedure (lambda-parameters exp)
+;                         (lambda-body exp)
+;                         env))
+;        ((begin? exp)
+;         (eval-sequence (begin-actions exp) env))
+;        ((cond? exp) (eval (cond->if exp) env))
+;        ((application? exp)
+;         (apply (eval (operator exp) env)
+;                (list-of-values (operands exp) env)))
+;        (else
+;          (error "Unknown expression type -- EVAL" exp))))
 
 
 
@@ -534,6 +534,7 @@
         ((variable? exp) (analyze-variable exp))
         ((assignment? exp) (analyze-assignment exp))
         ((definition? exp) (analyze-definition exp))
+        ((let? exp) (analyze (let->combination exp)))
         ((if? exp) (analyze-if exp))
         ((lambda? exp) (analyze-lambda exp))
         ((begin? exp) (analyze-sequence (begin-actions exp)))
@@ -686,11 +687,12 @@
 ;; 例. ２つの整数のリストがあり, そこから1個ずつとった数の和が素数になるかどうか調べる.
 ;;     フィルタする方法を3章で学んだ.
 ;;     非決定性計算は, 要求されることをプログラムに書き下す形で実現する.
-(define (prime-sum-pair list1 list2)
-  (let ((a (an-element-of list1))
-        (b (an-element-of list2)))
-    (require (prime? (+ a b)))
-    (list a b)))
+
+;    (define (prime-sum-pair list1 list2)
+;      (let ((a (an-element-of list1))
+;            (b (an-element-of list2)))
+;        (require (prime? (+ a b)))
+;        (list a b)))
 
 ;; 動き方イメージ (prime?は実装済と想定)
 ;; (prime-sum-pair '(1 3 5 8) '(20 35 110))
@@ -728,155 +730,155 @@
 
 ;; => q4.35.scm, q4.36.scm, q4.37.scm
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 4.3.2. 非決定性プログラムの例
-
-;; amb使えば論理パズル解けるよー (ええからはよ実装せえや)
-;; 問題を論理的に書き下せる時点で解けとるようなもんやん
-(define (multiple-dwelling)
-  (let ((baker (amb 1 2 3 4 5))
-        (cooper (amb 1 2 3 4 5))
-        (fletcher (amb 1 2 3 4 5))
-        (miller (amb 1 2 3 4 5))
-        (smith (amb 1 2 3 4 5)))
-    (require
-      (distinct? (list baker cooper fletcher miller smith)))
-    (require (not (= baker 5)))
-    (require (not (= cooper 5)))
-    (require (not (= fletcher 5)))
-    (require (not (= fletcher 1)))
-    (require (> miller cooper))
-    (require (not (= (abs (- smith fletcher)) 1)))
-    (require (not (= (abs (- fletcher cooper)) 1)))
-    (list (list 'baker baker)
-          (list 'cooper cooper)
-          (list 'fletcher fletcher)
-          (list 'miller miller)
-          (list 'smith smith))))
-
-;; => q4.38.scm, q4.39.scm, q4.40.scm, q4.41.scm, q4.42.scm, q4.43.scm, q4.44.scm
-
-
-;;; 自然言語の構文解析 ;;;
-;; (ええからはよ実装せえや)
-
-;; 品詞に分解
-(define nouns '(noun student professor cat class))
-(define verbs '(verbs studies lectures eats sleeps))
-(define articles '(articles the a))
-
-;; 文法.
-;; The cat eats を構文解析すると
-(sentence (noun-phrase (article the) (noun cat))
-          (verb eats))
-
-;; 道具箱
-(define (parse-sentence)
-  (list 'sentence
-        (parse-noun-phrase)
-        (parse-word verbs)))
-
-(define (parse-noun-phrase)
-  (list 'noun-phrase
-        (parse-word articles)
-        (parse-word nouns)))
-
-(define (parse-word word-list)
-  (require (not (null? *unparsed*)))
-  (require (memq (car *unparsed*) (cdr word-list)))
-  (let ((found-word (car *unparsed*)))
-    (set! *unparsed* (cdr *unparsed*))
-    (list (car word-list) found-word)))
-
-
-;; こうやって使う
-(define *unparsed* '())
-
-(define (parse input)
-  (set! *unparsed* input)
-  (let ((sent (parse-sentence)))
-    (require (null? *unparsed*))
-    sent))
-
-;; 探索とバックトラックは複雑な文法を扱うとき本当に有効である(ええからはよ実装せえや)
-(define prepositions '(prep for to in by with))
-
-(define (parse-prepositional-phrase)
-  (list 'prep-phrase
-        (parse-word prepositions)
-        (parse-noun-phrase)))
-
-(define (parse-sentence)
-  (list 'sentence
-        (parse-noun-phrase)
-        (parse-verb-phrase)))
-
-;; ようやくamb出てきた
-(define (parse-verb-phrase)
-  (define (maybe-extend verb-phrase)
-    (amb verb-phrase
-         (maybe-extend (list 'verb-phrase
-                             verb-phrase
-                             (parse-prepositional-phrase)))))
-  (maybe-extend (parse-word verbs)))
-
-;; ついでに名刺句の定義を改善する
-(define (parse-simple-noun-phrase)
-  (list 'simple-noun-phrase
-        (parse-word articles)
-        (parse-word nouns)))
-
-(define (parse-noun-phrase)
-  (define (maybe-extend noun-phrase)
-    (amb noun-phrase
-         (maybe-extend (list 'noun-phrase
-                             noun-phrase
-                             (parse-prepositional-phrase)))))
-  (maybe-extend (parse-simple-noun-phrase)))
-
-;; ここまでやると, 以下のような複雑な文を解析できる(うれしげに言うとらんと実装せえっちゅうに)
-(parse '(the student with the cat sleeps in the class))
-;; =>
-(sentence
-  (noun-phrase
-    (simple-noun-phrase (articles the) (noun student))
-    (prep-phrase (prep with)
-                 (simple-noun-phrase
-                   (article the) (noun cat))))
-  (verb-phrase
-    (verb sleeps)
-    (prep-phrase (prep in)
-                 (simple-noun-phrase
-                   (article the) (noun class)))))
-
-;; 別の例
-(parse '(the professor lectures to the student with the cat))
-;; =>
-(sentence
-  (simple-noun-phrase (article the) (noun professor))
-  (verb-phrase
-    (verb-phrase
-      (verb lectures)
-      (prep-phrase (prep to)
-                   (simple-noun-phrase
-                     (article the) (noun student))))
-    (prep-phrase (prep with)
-                 (simple-noun-phrase
-                   (article the) (noun cat)))))
-;; => 再実行
-(sentence
-  (simple-noun-phrase (article the) (noun professor))
-  (verb-phrase
-    (verb lectures)
-    (prep-phrase (prep to)
-                 (noun-phrase
-                   (simple-noun-phrase
-                     (article the) (noun student))
-                   (prep-phrase (prep with)
-                                (simple-noun-phrase
-                                  (article the) (noun cat)))))))
-
-;; => q4.45.scm, q4.46.scm, q4.47.scm, q4.48.scm, q4.49.scm
+;        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;        ;; 4.3.2. 非決定性プログラムの例
+;
+;        ;; amb使えば論理パズル解けるよー (ええからはよ実装せえや)
+;        ;; 問題を論理的に書き下せる時点で解けとるようなもんやん
+;        (define (multiple-dwelling)
+;          (let ((baker (amb 1 2 3 4 5))
+;                (cooper (amb 1 2 3 4 5))
+;                (fletcher (amb 1 2 3 4 5))
+;                (miller (amb 1 2 3 4 5))
+;                (smith (amb 1 2 3 4 5)))
+;            (require
+;              (distinct? (list baker cooper fletcher miller smith)))
+;            (require (not (= baker 5)))
+;            (require (not (= cooper 5)))
+;            (require (not (= fletcher 5)))
+;            (require (not (= fletcher 1)))
+;            (require (> miller cooper))
+;            (require (not (= (abs (- smith fletcher)) 1)))
+;            (require (not (= (abs (- fletcher cooper)) 1)))
+;            (list (list 'baker baker)
+;                  (list 'cooper cooper)
+;                  (list 'fletcher fletcher)
+;                  (list 'miller miller)
+;                  (list 'smith smith))))
+;
+;        ;; => q4.38.scm, q4.39.scm, q4.40.scm, q4.41.scm, q4.42.scm, q4.43.scm, q4.44.scm
+;
+;
+;        ;;; 自然言語の構文解析 ;;;
+;        ;; (ええからはよ実装せえや)
+;
+;        ;; 品詞に分解
+;        (define nouns '(noun student professor cat class))
+;        (define verbs '(verbs studies lectures eats sleeps))
+;        (define articles '(articles the a))
+;
+;        ;; 文法.
+;        ;; The cat eats を構文解析すると
+;        (sentence (noun-phrase (article the) (noun cat))
+;                  (verb eats))
+;
+;        ;; 道具箱
+;        (define (parse-sentence)
+;          (list 'sentence
+;                (parse-noun-phrase)
+;                (parse-word verbs)))
+;
+;        (define (parse-noun-phrase)
+;          (list 'noun-phrase
+;                (parse-word articles)
+;                (parse-word nouns)))
+;
+;        (define (parse-word word-list)
+;          (require (not (null? *unparsed*)))
+;          (require (memq (car *unparsed*) (cdr word-list)))
+;          (let ((found-word (car *unparsed*)))
+;            (set! *unparsed* (cdr *unparsed*))
+;            (list (car word-list) found-word)))
+;
+;
+;        ;; こうやって使う
+;        (define *unparsed* '())
+;
+;        (define (parse input)
+;          (set! *unparsed* input)
+;          (let ((sent (parse-sentence)))
+;            (require (null? *unparsed*))
+;            sent))
+;
+;        ;; 探索とバックトラックは複雑な文法を扱うとき本当に有効である(ええからはよ実装せえや)
+;        (define prepositions '(prep for to in by with))
+;
+;        (define (parse-prepositional-phrase)
+;          (list 'prep-phrase
+;                (parse-word prepositions)
+;                (parse-noun-phrase)))
+;
+;        (define (parse-sentence)
+;          (list 'sentence
+;                (parse-noun-phrase)
+;                (parse-verb-phrase)))
+;
+;        ;; ようやくamb出てきた
+;        (define (parse-verb-phrase)
+;          (define (maybe-extend verb-phrase)
+;            (amb verb-phrase
+;                 (maybe-extend (list 'verb-phrase
+;                                     verb-phrase
+;                                     (parse-prepositional-phrase)))))
+;          (maybe-extend (parse-word verbs)))
+;
+;        ;; ついでに名刺句の定義を改善する
+;        (define (parse-simple-noun-phrase)
+;          (list 'simple-noun-phrase
+;                (parse-word articles)
+;                (parse-word nouns)))
+;
+;        (define (parse-noun-phrase)
+;          (define (maybe-extend noun-phrase)
+;            (amb noun-phrase
+;                 (maybe-extend (list 'noun-phrase
+;                                     noun-phrase
+;                                     (parse-prepositional-phrase)))))
+;          (maybe-extend (parse-simple-noun-phrase)))
+;
+;        ;; ここまでやると, 以下のような複雑な文を解析できる(うれしげに言うとらんと実装せえっちゅうに)
+;        (parse '(the student with the cat sleeps in the class))
+;        ;; =>
+;        (sentence
+;          (noun-phrase
+;            (simple-noun-phrase (articles the) (noun student))
+;            (prep-phrase (prep with)
+;                         (simple-noun-phrase
+;                           (article the) (noun cat))))
+;          (verb-phrase
+;            (verb sleeps)
+;            (prep-phrase (prep in)
+;                         (simple-noun-phrase
+;                           (article the) (noun class)))))
+;
+;        ;; 別の例
+;        (parse '(the professor lectures to the student with the cat))
+;        ;; =>
+;        (sentence
+;          (simple-noun-phrase (article the) (noun professor))
+;          (verb-phrase
+;            (verb-phrase
+;              (verb lectures)
+;              (prep-phrase (prep to)
+;                           (simple-noun-phrase
+;                             (article the) (noun student))))
+;            (prep-phrase (prep with)
+;                         (simple-noun-phrase
+;                           (article the) (noun cat)))))
+;        ;; => 再実行
+;        (sentence
+;          (simple-noun-phrase (article the) (noun professor))
+;          (verb-phrase
+;            (verb lectures)
+;            (prep-phrase (prep to)
+;                         (noun-phrase
+;                           (simple-noun-phrase
+;                             (article the) (noun student))
+;                           (prep-phrase (prep with)
+;                                        (simple-noun-phrase
+;                                          (article the) (noun cat)))))))
+;
+;        ;; => q4.45.scm, q4.46.scm, q4.47.scm, q4.48.scm, q4.49.scm
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -942,4 +944,16 @@
       (try-next cprocs))))
 
 ;; => q4.52.scm, q4.53.scm, q4.54.scm
+
+
+;; test
+; ;; analyze版でletを使えるようにしないといけない
+; (driver-loop)
+; (define count 0)
+; (let ((x (an-element-of '(a b c)))
+;       (y (an-element-of '(a b c))))
+;   (set! count (+ count 1))
+;   (require (not (eq? x y)))
+;   (list x y count))
+
 
