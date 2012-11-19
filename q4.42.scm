dd1082221df2bd17f2c08010e2a18b1b0e72fb6f
Hash@nona <takuya21hashimoto@gmail.com>
2012-11-19 18:53:46 +0900

    copy & paste from Zhang's Blog (review

---
+++ q4.42.scm
@@ -0,0 +1,23 @@
+;; http://wqzhang.wordpress.com/2010/04/27/sicp-exercise-4-42/
+;; コピペ
+(define (liars)
+  (define (xor a b)
+    (or (and a (not b)) (and (not a) b)))
+  (let ((betty (amb 1 2 3 4 5))
+        (ethel (amb 1 2 3 4 5))
+        (joan (amb 1 2 3 4 5))
+        (kitty (amb 1 2 3 4 5))
+        (mary (amb 1 2 3 4 5)))
+    (require
+     (distinct? (list betty ethel joan kitty mary)))
+    (require (xor (= kitty 2) (= betty 3)))
+    (require (xor (= ethel 1) (= joan 2)))
+    (require (xor (= joan 3) (= ethel 5)))
+    (require (xor (= kitty 2) (= mary 4)))
+    (require (xor (= mary 4) (= betty 1)))
+    (list (list 'betty betty)
+          (list 'ethel ethel)
+          (list 'joan joan)
+          (list 'kitty kitty)
+          (list 'mary mary))))
+
---
+++ q4.43.scm
@@ -0,0 +1,39 @@
+;; http://wqzhang.wordpress.com/2010/04/28/sicp-exercise-4-43/
+;; コピペ
+
+(define (yacht-puzzle)
+; father = (last-name daughter yacht)
+  (define (last-name f) (car f))
+  (define (daughter f) (cadr f))
+  (define (yacht f) (caddr f))
+  (define (different? f) (not (eq? (daughter f) (yacht f))))
+  (define (father-of d fathers)
+    (if (eq? d (daughter (car fathers)))
+        (car fathers)
+        (father-of d (cdr fathers))))
+  (let ((Downing (list 'Downing
+                       (amb 'Gabrielle 'Lorna 'Mary 'Rosalind)
+                       'Melissa))
+        (Hall (list 'Hall
+                    (amb 'Gabrielle 'Lorna 'Mary 'Melissa)
+                    'Rosalind))
+        (Hood (list 'Hood 'Melissa 'Gabrielle))
+        (Moore (list 'Moore 'Mary 'Lorna))
+        (Parker (list 'Parker
+                      (amb 'Gabrielle 'Lorna 'Mary 'Melissa
+                           'Rosalind)
+                      (amb 'Gabrielle 'Lorna 'Mary 'Melissa
+                           'Rosalind))))
+    (require (different? Parker))
+    (let ((fathers (list Downing Hall Hood Moore Parker)))
+      (define (map proc a)
+        (if (null? a)
+            '()
+            (cons (proc (car a)) (map proc (cdr a)))))
+      (let ((daughters (map daughter fathers)))
+        (require (distinct? daughters))
+        (let ((yachts (map yacht fathers)))
+          (require (distinct? yachts))
+          (require (eq? (daughter Parker)
+                        (yacht (father-of 'Gabrielle fathers))))
+          fathers)))))
