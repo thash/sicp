17a0d30a39f104176cb0435f75261251915e8a2b
Hash <takuya21hashimoto@gmail.com>
2012-12-03 08:25:59 +0900

    use Serendip's Logic Programming code for now

---
+++ sec3/table.scm
@@ -0,0 +1,36 @@
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+          (let ((record (assoc key-2 (cdr subtable))))
+            (if record
+              (cdr record)
+              #f))
+          #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+          (let ((record (assoc key-2 (cdr subtable))))
+            (if record
+              (set-cdr! record value)
+              (set-cdr! subtable
+                        (cons (cons key-2 value)
+                              (cdr subtable)))))
+          (set-cdr! local-table
+                    (cons (list key-1
+                                (cons key-2 value))
+                          (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup) lookup)
+            ((eq? m 'insert!) insert!)
+            (else (display "Unknown operationnnn -- TABLE")
+                  (display m))))
+    dispatch))
+
+;; usage:
+(define table (make-table))
+(define get (table 'lookup))
+(define put (table 'insert!))
+
