d12b1057d04f34b2913be063964a784f15215908
Hash <takuya21hashimoto@gmail.com>
2013-01-03 20:57:50 +0900

    REVIEW: q2.35 (and looked throug q2.36) -- accumulate
    [5acb58c] need review: q2.35
    [78d6bf1] need review: q2.36.scm

--- sec2/q2.35.scm
+++ sec2/q2.35.scm
@@ -7,7 +7,6 @@
     (op (car sequence)
         (accumulate op initial (cdr sequence)))))
 
-
 (define (count-leaves t)
   (accumulate
     (lambda (leaves total) (+ leaves total)) ; 適用operationはtotalへの足し合わせ
@@ -31,4 +30,51 @@
                                (else (accumulate + 0 subtree)))))))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 正確な作用順じゃないと思うけど展開してみる.
 
+(define x (list 1 2 3))
+
+(count-leaves x)
+
+(accumulate (lambda (leaves total) (+ leaves total))
+            0
+            (map (lambda (sub-tree) (if (pair? sub-tree) (count-leaves sub-tree) 1))
+                 x))
+
+(accumulate (lambda (leaves total) (+ leaves total))
+            0
+            (map (lambda (sub-tree) (if (pair? sub-tree) (count-leaves sub-tree) 1))
+                 '(1 2 3)))
+
+(accumulate (lambda (leaves total) (+ leaves total))
+            0
+            '(1 1 1)) ; mapにより sub-tree = 1 として3個実行され, すべてpair? => #fなので1
+
+;; accumulateに渡すべきsequenceができたので, 次はaccumulateの中身.
+;; '(1 1 1) != null なのでop が実行される.
+(op (car '(1 1 1))
+    (accumulate op 0 '(1 1)))
+
+(op (car '(1 1 1))
+    (op (car '(1 1))
+        (accumulate op 0 '(1))))
+
+(op (car '(1 1 1))
+    (op (car '(1 1))
+        (op (car '(1))
+            (accumulate op 0 '()))))
+
+(op (car '(1 1 1))
+    (op (car '(1 1))
+        (op (car '(1))
+            0))) ; null?に該当しようやく再帰が止まる.
+
+;; carを開く
+(op 1 (op 1 (op 1 0)))
+
+;; 今回はop = leaves,totalという引数付きで定義されたlambdaなので
+(lambda 1 (lambda 1 (lambda 1 0)))
+; で, lambdaの本体 => (+ arg1 arg2) ... lambda作用時の表記がわからん
+(+ 1 (+ 1 (+ 1 0)))
+3
