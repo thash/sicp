546d584fd09e9fd15aee3d209b0e3f0ee43dad59
Hash <takuya21hashimoto@gmail.com>
2013-01-04 14:32:45 +0900

    convert to UTF8

--- sec3/q3.18.scm
+++ sec3/q3.18.scm
@@ -5,5 +5,4 @@
             ((memq x stored) #t)
             (else (set! stored (cons x stored))
                   (cdr-looping? (cdr x)))))))
-
-
+;; cdrはpointerだから, pointerが既出なら間違いなくloopと考えて良いのか
