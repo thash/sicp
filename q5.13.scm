4b0fca15bdfb9996664f7ea00e89ca6a17cfe7df
Hash <takuya21hashimoto@gmail.com>
2013-01-22 14:40:30 +0900

    q5.13 yet another solution -- make new register while lookuping

--- q5.13.scm
+++ q5.13.scm
@@ -1,11 +1,6 @@
 ;; > make-machineの引数としてレジスタリストを要求するのではなく, 制御器の命令列を使って計算機の持つレジスタを決めるようにシミュレータを設計せよ.
 ;; > make-machineであらかじめレジスタを割り当てる代わりに, 命令のアセンブリ中に, 初めて見る度に, レジスタを割り当てることができる.
 
-;; update-insts!実行中, レジスタを参照する命令が出てくるたびに新規かどうか判定して新規であればレジスタを割り当てる
-;; レジスタを参照する命令は assign, reg, save, restore の4つ
-;; このうちregのみ, レジスタ名が引数部分に出てくることに注意
-
-;; NEXT-LOOP: 別解. update-insts!でallocateするのではなく, lookupで失敗したら登録するLazyな方法
 
 (define (make-machine ops controller-text) ;; register-names を引数から削除
   (let ((machine (make-new-machine)))
@@ -19,6 +14,11 @@
     machine))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 解法(1): 命令のアセンブリ中に, 初めて見る度に, レジスタを割り当てる
+;;   update-insts!実行中レジスタを参照する命令が出てくるたびに新規かどうか判定, 新規であればレジスタを割り当て
+;;   レジスタを参照する命令は assign, reg, save, restore の4つ
+;;   このうちregのみ, レジスタ名が引数部分に出てくることに注意
 (define (unique lst)
   (cond ((null? lst) lst)
         ((member (car lst) (cdr lst))
@@ -71,3 +71,62 @@
             (instruction-text inst) labels machine
             pc flag stack ops)))
       insts)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 解法(2): update-insts!でallocateするのではなく, lookupで失敗したら登録するLazyな方法
+;; こっちのがかっこいい
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '()))
+    (let ((the-ops
+            (list (list 'initialize-stack
+                        (lambda () (stack 'initialize)))))
+          (register-table
+            (list (list 'pc pc) (list 'flag flag))))
+
+      ;;; 削除
+      ;; (define (allocate-register name)
+      ;;   (if (assoc name register-table)
+      ;;     (error "Multiply defined register: " name)
+      ;;     (set! register-table
+      ;;       (cons (list name (make-register name))
+      ;;             register-table)))
+      ;;   'register-allocated)
+
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+            (cadr val)
+            ;; 変更 -- 存在しなかった時にエラーにするのではなく
+            ;;         register新規追加してもう一度lookup
+            (begin
+              (set! register-table
+                (cons (list name (make-register name))
+                      register-table))
+              (lookup-register name)))))
+
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+            'done
+            (begin
+              ((instruction-execution-proc (car insts)))
+              (execute)))))
+
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ;; ((eq? message 'allocate-register) allocate-register) ; 削除
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
